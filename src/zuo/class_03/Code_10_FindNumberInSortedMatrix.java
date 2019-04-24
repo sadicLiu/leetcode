09e94412f33e3f3758815530b30912b182e6dfb9
sadicLiu <190978752@qq.com>
2019-04-24 22:03:45 +0800

    add find number in sorted matrix code

---
+++ src/zuo/class_03/Code_10_FindNumberInSortedMatrix.java
@@ -0,0 +1,33 @@
+package zuo.class_03;
+
+public class Code_10_FindNumberInSortedMatrix {
+    public static boolean findNumber(int[][] matrix, int num) {
+        if (matrix == null)
+            return false;
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == num)
+                return true;
+            else if (matrix[row][col] < num)
+                row++;
+            else
+                col--;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{0, 1, 2, 3, 4, 5, 6},// 0
+                {10, 12, 13, 15, 16, 17, 18},// 1
+                {23, 24, 25, 26, 27, 28, 29},// 2
+                {44, 45, 46, 47, 48, 49, 50},// 3
+                {65, 66, 67, 68, 69, 70, 71},// 4
+                {96, 97, 98, 99, 100, 111, 122},// 5
+                {166, 176, 186, 187, 190, 195, 200},// 6
+                {233, 243, 321, 341, 356, 370, 380} // 7
+        };
+        int K = 233;
+        System.out.println(findNumber(matrix, K));
+    }
+}
